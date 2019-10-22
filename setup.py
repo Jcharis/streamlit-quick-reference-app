0a357daf92767d6cb7c46cec7d6f953a85a5a855
JCharis <jcharistech@gmail.com>
2019-10-22 20:14:03 +0300

    Updated Profile and Setup

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"jcharistech@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
