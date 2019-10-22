d05454670605846c02d3b1a3d6ca3e017f2c2330
JCharis <jcharistech@gmail.com>
2019-10-22 19:32:52 +0300

    Streamlit App Deployment on Heroku

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+def main():
+	"""Streamlit Quick Reference App """
+
+	st.title("Streamlit Quick Reference App")
+	st.header("Streamlit Is Awesome")
+
+
+
+
+if __name__ == '__main__':
+	main()
